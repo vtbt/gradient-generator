f232965264514c568b4816a8362fdd02df1a700d
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2024-12-01 17:54:57 +0700

    Finish exercise - add and remove color

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,67 @@
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
+
+const DEFAULT_COLOR = '#FF0000';
+
 function App() {
   const [colors, setColors] = useState(['#FFD500', '#FF0040']);
+  const [isDisabledRemove, setIsDisabledRemove] = useState(false);
+  const [isDisabledAdd, setIsDisabledAdd] = useState(false);
 
   const colorStops = colors.join(', ');
   const backgroundImage = `linear-gradient(${colorStops})`;
 
+  useEffect(() => {
+    if (colors.length === 5) {
+      setIsDisabledAdd(true);
+      return;
+    }
+    if (colors.length === 2) {
+      setIsDisabledRemove(true);
+      return;
+    }
+  }, [colors.length]);
+
+  const handleRemoveColor = () => {
+    if (colors.length === 2) {
+      return;
+    }
+
+    setIsDisabledAdd(false);
+    const newColors = [...colors];
+    newColors.pop();
+    setColors(newColors);
+  };
+
+  const handleAddColor = () => {
+    if (colors.length === 5) {
+      return;
+    }
+
+    setIsDisabledRemove(false);
+    setColors([...colors, DEFAULT_COLOR]);
+  };
+
+  const handleChangeColor = (
+    event: ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const newColors = [...colors];
+    newColors[index] = event.target.value;
+    setColors(newColors);
+  };
+
   return (
-    <>
+    <div className="wrapper">
+      <div className="actions">
+        <button disabled={isDisabledRemove} onClick={handleRemoveColor}>
+          Remove color
+        </button>
+        <button disabled={isDisabledAdd} onClick={handleAddColor}>
+          Add color
+        </button>
+      </div>
+
       <div
         className="gradient-preview"
         style={{
@@ -15,28 +69,25 @@ function App() {
         }}
       />
 
-      <form>
+      <div className="colors">
         {colors.map((color, index) => {
           const colorId = `color-${index}`;
-
           return (
-            <div key={colorId} className="color-row">
+            <div key={colorId} className="color-wrapper">
               <label htmlFor={colorId}>Color {index + 1}:</label>
-              <input
-                id={colorId}
-                type="color"
-                value={color}
-                onChange={(event) => {
-                  const newColors = [...colors];
-                  newColors[index] = event.target.value;
-                  setColors(newColors);
-                }}
-              />
+              <div className="input-wrapper">
+                <input
+                  id={colorId}
+                  type="color"
+                  onChange={(event) => handleChangeColor(event, index)}
+                  value={color}
+                />
+              </div>
             </div>
           );
         })}
-      </form>
-    </>
+      </div>
+    </div>
   );
 }
 
